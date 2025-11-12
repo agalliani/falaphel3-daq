6e3d525570bcf8f981705c49baf4676ef41cbb5e
microlab agalliani <andrea.galliani@unibg.it>
2025-11-12 17:15:39 +0100

    nuovi dati

--- software_daq/main.py
+++ software_daq/main.py
@@ -67,8 +67,8 @@ class FpgaControlApp:
         self.num_injections = tk.IntVar(value=31) # Numero di iniezioni per step
 
         # Variabili per l'iniezione su singolo pixel
-        self.inj_pixel_x = tk.IntVar(value=5)
-        self.inj_pixel_y = tk.IntVar(value=5)
+        self.inj_pixel_x = tk.IntVar(value=6)
+        self.inj_pixel_y = tk.IntVar(value=6)
 
         # VARIABILI PER LA CONFIGURAZIONE DEL PIXEL
         self.config_cap50 = tk.IntVar(value=1)
@@ -76,7 +76,7 @@ class FpgaControlApp:
         self.config_cap_csa_load = tk.IntVar(value=0)
         self.config_dac_th = tk.IntVar(value=0)
         self.config_test_en = tk.IntVar(value=1)
-        self.config_t_up = tk.IntVar(value=0)
+        self.config_t_up = tk.IntVar(value=1)
         self.config_out_en = tk.IntVar(value=1)
 
         # VARIABILI PER LA SOTTOMATRICE SCAN
