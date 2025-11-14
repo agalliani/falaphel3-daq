32dbc7b56175883b4f72e01bda96467aca46e70b
agalliani <andrea.galliani.29@gmail.com>
2025-11-14 15:20:01 +0100

    Refactor batch_config.yaml and batch_runner.py to integrate injection settings into task execution

--- software_daq/batch_runner.py
+++ software_daq/batch_runner.py
@@ -40,24 +40,57 @@ class BatchRunner:
         # Uso del metodo engine tramite self.engine
         self.engine.connect_power_supply() 
         self.engine._prepare_power_supply_for_sweep() 
+
+
+
+
         # 3. Esecuzione dei task
         for task in self.cfg["tasks"]:
             op = task["operation"]
 
             if op == "sweep_pixel":
                 p = task
-                print(f"Running sweep: {p['name']}")
 
-                print(f"Sweep params: {p['sweep']}")
-                print(f"Pixel config params: {p['config']}")
                 sweep_params = p["sweep"]
                 pixel_cfg = p["config"]
+                injection_cfg = p["injection"]
 
+                pad_word = self.engine.asic_config.get_init_pad_string()
+                pointer_word = self.engine.asic_config.get_pixel_pointer_selection(x_5b=self.inj_pixel_x.get(), y_3b=self.inj_pixel_y.get())
+
+                config_pixel_word = self.engine.asic_config.get_config_pointed_pixel(
+                    cap25_1b=pixel_cfg["cap25"], dac_th_5b=pixel_cfg["dac_th"], test_en_1b=pixel_cfg["test_en"], 
+                    cap50_1b=pixel_cfg["cap50"], cap_csa_load_1b=pixel_cfg["cap_csa_load"], 
+                    t_up_1b=pixel_cfg["t_up"], out_en_1b=pixel_cfg["out_en"]
+                )
+
+                inj_word1_start, inj_word2_start = self.engine.asic_config.get_injection_settings(
+                    bypass_1b=injection_cfg["bypass"], period_8b=injection_cfg["period"], burst_8b=injection_cfg["burst"], duty_4b=injection_cfg["duty"], start_1b=1
+                )
+                inj_word1_stop, inj_word2_stop = self.engine.asic_config.get_injection_settings(
+                    bypass_1b=injection_cfg["bypass"], period_8b=injection_cfg["period"], burst_8b=injection_cfg["burst"], duty_4b=injection_cfg["duty"], start_1b=0
+                )
+                tot_request = self.asic_config.get_save_tot_command()
+                toa_request = self.asic_config.get_save_toa_command()
+
+                
+                binary_command_params = {
+                    'pad_word': pad_word,
+                    'pointer_word': pointer_word,
+                    'config_pixel_word': config_pixel_word,
+                    'inj_word1_start': inj_word1_start,
+                    'inj_word2_start': inj_word2_start,
+                    'inj_word1_stop': inj_word1_stop,
+                    'inj_word2_stop': inj_word2_stop,
+                    'tot_request': tot_request,
+                    'toa_request': toa_request
+                }
+                
                 # Uso del metodo engine tramite self.engine
                 self.engine.perform_sweep(
                     port, baud,
                     sweep_params=sweep_params,
-                    binary_command_params=None,
+                    binary_command_params=binary_command_params,
                     pixel_config_params=pixel_cfg,
                     isMatrixScan=False
                 )
